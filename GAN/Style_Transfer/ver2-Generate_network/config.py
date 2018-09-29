209138de597685502cb4c6ea4b9f73f64bec84f6
younginsong21 <younginsong21@naver.com>
2018-09-29 10:55:48 +0900

    modify configuration

--- GAN/Style_Transfer/ver2-Generate_network/config.py
+++ GAN/Style_Transfer/ver2-Generate_network/config.py
@@ -6,7 +6,7 @@ def get_config():
     # data preprocessing hyper-parameters
     parser.add_argument('--dataroot', type=str, default="dataset")
     parser.add_argument('--content_size', type=int, default=256)
-    parser.add_argument('--style_path', type=str, default='styles/ghibli.png')
+    parser.add_argument('--style_path', type=str, default='styles/cropped2.png')
     parser.add_argument('--style_size', type=int, default=None)
     parser.add_argument('--batch_size', type=int, default=4)
 
@@ -16,10 +16,10 @@ def get_config():
     parser.add_argument('--sample_interval', type=int, default=2, help="epoch interval to sample output image")
     parser.add_argument('--checkpoint_interval', type=int, default=10, help="epoch interval to sample model checkpoint")
 
-    parser.add_argument('--style_weight', type=float, default=1e10, help="weight for style loss, default is 1e10")
+    parser.add_argument('--style_weight', type=float, default=2e10, help="weight for style loss, default is 1e10")
     parser.add_argument('--content_weight', type=float, default=1e5, help="weight for content loss, default is 1e5")
 
-    parser.add_argument('--lr', type=float, default=0.003)
+    parser.add_argument('--lr', type=float, default=0.005)
     parser.add_argument('--training', type=str, default="")
     parser.add_argument('--sample_folder', default=None)
 
